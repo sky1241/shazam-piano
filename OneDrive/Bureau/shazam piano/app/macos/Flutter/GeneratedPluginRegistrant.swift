5e0b724cca0fda8e7b783954afd5f39fb077b4f9
sky1241 <ludovik1241@gmail.com>
2025-12-21 05:20:37 +0100

    Practice mode: add auto-calibration beep and latency compensation

--- OneDrive/Bureau/shazam piano/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ OneDrive/Bureau/shazam piano/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import cloud_firestore
 import firebase_analytics
 import firebase_auth
@@ -20,6 +21,7 @@ import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
