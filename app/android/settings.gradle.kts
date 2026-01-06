7055acef4b23caa9fb74406af93f637dc7d533d1
sky1241 <ludovik1241@gmail.com>
2026-01-06 16:02:10 +0100

    Fix: Add Alibaba mirrors to settings.gradle.kts with dependencyResolutionManagement

--- app/android/settings.gradle.kts
+++ app/android/settings.gradle.kts
@@ -11,8 +11,18 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
+        mavenCentral()
     }
 }
 
